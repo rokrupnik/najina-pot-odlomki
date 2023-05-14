8ca0ea1d108cc9df5716337180f5de3c6cab1199
rupnik <rok.rupnik@rls.si>
2023-05-14 22:38:12 +0200

    Added netlify function proxy feature

--- functions/biblija-net-proxy.js
+++ functions/biblija-net-proxy.js
@@ -1,9 +1,28 @@
 const fetch = require('node-fetch')
 
 exports.handler = async event => {
-    const passage = event.queryStringParameters.passage || 'World'
+    const passage = event.queryStringParameters.passage || ''
+    passage = encodeURI(passage)
 
-    
+    let statusCode, html;
+
+    try {
+        const response = await fetch(`https://www.biblija.net/biblija.cgi?m=${passage}&q=&idq=60&id59=1&pos=0&set=26&l=sl3`);
+        html = await response.text();
+        statusCode = 200;
+    } catch (err) {
+        statusCode = err.statusCode || 500;
+        html = err.message;
+    }
+
+    return {
+        statusCode,
+        body: html
+    }
+
+        
+
+
 
     return {
         statusCode: 200,
