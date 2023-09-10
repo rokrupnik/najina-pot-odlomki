1e52f148f9297aec9f4f03fbe5f377469305152f
rupnik <rok.rupnik@rls.si>
2023-09-10 22:43:55 +0200

    Added biblija.net link

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,8 @@ function App() {
     // var myHeaders = new Headers();
     // myHeaders.append('Content-Type','text/html; charset=windows-1250');
 
+    
+
     fetch('/.netlify/functions/biblija-net-proxy?passage=' + encodeURIComponent(passage) )
       .then(res => res.text())
       .then(html => {
@@ -82,6 +84,15 @@ function App() {
       {passage
         ?<div className="passage" dangerouslySetInnerHTML={{__html: passageHtml}}></div>
         :<div className="passage">No passage selected</div>
+      }
+      {passage &&
+        <a
+          className="bn-link" href={`https://www.biblija.net/biblija.cgi?m=${encodeURI(passage)}&q=&idq=60&id59=1&pos=0&set=26&l=sl3`}
+          target="_blank"
+        >
+            Biblija.net link
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{height: '1.4em', marginLeft: '0.25em', top: '5px', position: 'relative'}}><path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" /></svg>
+        </a>
       }
       
     </>
