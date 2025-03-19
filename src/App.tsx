d43c913b0c93edbfa7fb9040b58f8714d900b2af
rokrupnik <rok7rupnik@gmail.com>
2025-03-19 13:24:57 +0100

    SVG arrow icons

--- src/App.tsx
+++ src/App.tsx
@@ -130,7 +130,7 @@ function App() {
       {/* <h3>{ passage }</h3> */}
       <div className="card">
         <button onClick={() => handleClick(-1)} disabled={index === 0}>
-          ←
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ height: '1.4em', marginLeft: '0.25em', top: '5px', position: 'relative' }}><path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" /></svg>
         </button>
 
         <button onClick={handleCurrentMonth} style={{ marginInline: '20px' }}>
@@ -138,7 +138,7 @@ function App() {
         </button>
         
         <button onClick={() => handleClick(1)} disabled={index === passages.length - 1}>
-          →
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ height: '1.4em', marginLeft: '0.25em', top: '5px', position: 'relative' }}><path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" /></svg>
         </button>
 
         {passage ? <p className="passage-label"><strong>{passage}</strong><br></br>{month}</p> : ''}
